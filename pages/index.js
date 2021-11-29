4ef2611138a65b13ab3ec4dc0ca7c71f97b0226e
kgoyan <kaingoyan@gmail.com>
2021-11-28 17:42:39 -0700

    finish

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { AppBar } from "components/appbar"
 import { GoogleProvider } from "components/authentication/providers"
 import { Title } from "ui/title"
 import { Spacer } from "ui/spacers"
-import { EmailAndPassword } from "components/authentication/login"
+import {UserLogin} from "components/authentication/user-login"
 import { Legal, HighLight } from "ui/legal"
 import {PageLayout, PageHeader, PageFooter, PageBody} from 'layouts/loginpage'
 function index (){
@@ -17,7 +17,7 @@ function index (){
                <PageBody>
                    <GoogleProvider>With Google</GoogleProvider>
                    <Spacer className="spacing" >OR</Spacer>
-                   <EmailAndPassword/>
+                   <UserLogin/>
                 </PageBody>             
                 <PageFooter>
                         <Legal>Legal Stuff  <HighLight>terms and conditions</HighLight></Legal>
