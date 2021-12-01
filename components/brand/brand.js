1990f1b8475fe13ae27bb1f7c78a74d05ec0b95c
kgoyan <kaingoyan@gmail.com>
2021-11-30 17:36:06 -0700

    FINISHED

--- components/brand/brand.js
+++ components/brand/brand.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Logo } from "components/icons/logo"
+import { Logo } from "components/icons"
 import {BrandStyles} from "./styles"
 
 
--- components/icons/logo/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Logo } from './logo'
-
- 
