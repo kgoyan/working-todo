af258627a88222477c0b6998353c79568ff04b00
kgoyan <kaingoyan@gmail.com>
2021-12-11 12:15:05 -0700

    lab5

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,74 @@
-import { AppBar } from "components/appbar"
-import { GitHubProvider, GoogleProvider } from "components/authentication/providers"
-import { Title } from "ui/title"
-import { Spacer } from "ui/spacers"
-import {UserLogin} from "components/authentication/user-login"
-import { Legal, HighLight } from "ui/legal"
-import {PageLayout, PageHeader, PageFooter, PageBody} from 'layouts/loginpage'
-function index (){
-    
-    return(
-        <>
-              <AppBar/>
-              <PageLayout>
-                    <PageHeader>
-                    <Title>Account Login</Title>
-                    </PageHeader>
-               <PageBody>
-                   <GoogleProvider style={{marginBottom:"1.5rem"}}>With Google</GoogleProvider>
-                   <GitHubProvider>With Github</GitHubProvider>
-                   <Spacer className="spacing" >OR</Spacer>
-                   <UserLogin/>
-                </PageBody>             
-                <PageFooter>
-                        <Legal>Legal Stuff  <HighLight>terms and conditions</HighLight></Legal>
-                </PageFooter>
+import Image from 'next/dist/client/image';
+import Link from 'next/link'
+import styled from 'styled-components';
+import { Brand } from 'components/brand';
+import { Button } from 'ui/buttons';
+import apple from 'public/apple.png'
 
-              </PageLayout>
-        </>
-    )
+
+const LinksContainer = styled.div`
+ max-width: 320px;
+margin: 2rem auto;
+display:flex;
+flex-direction: column;
+justify-content: center;
+button{
+  margin-bottom: 1rem;
+  font-size:1.125rem;
 }
+  
+`;
 
- 
+const PageHeader = styled.header`
+  text-align: center;
+  color:#222b37;
+  p{
+    margin:0.5rem;
 
-export default index
+  }
+  .brand{
+    margin-top:2rem;
+     h1{
+       margin-bottom: -2rem;
+     }
+  }
+  li{
+    color:#646f79;
+        font-weight: 200;
+        font-size: 1.5rem;
+      }
+
+`
+
+
+
+
+function index(props) {
+
+    return (
+        <>
+      
+         <PageHeader>
+        <Brand className="brand" width="148" size="5rem"   display="column"  />      
+    
+             <p>official task management tool for tools</p>
+             <LinksContainer>
+             <Link   href="/login">
+                 <Button bgcolor="#0fa9ec" color="#ffffff">new user account setup</Button>
+            </Link>
+             <Link href="/login">
+               <Button bgcolor="#0071e3" color="white">current user account login</Button>
+             </Link>
+            </LinksContainer>
+                <ul>              
+                  <li>No Credit Card Required</li>
+                  <li>Upgrade your account at anytime</li>
+                </ul>
+         </PageHeader>
+         
+        </>
+      );
+    }
+    
+    export default index;
+    
